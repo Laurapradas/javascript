77892f25905d665914ef2b39a700c8f76b6099f2
Laura Pradas <49239847+Laurapradas@users.noreply.github.com>
2019-07-04 10:02:02 +0200

    Script para multiplicar dos numeros
    funcion js que recibe 2 parametros, los multiplica y devuelve el valor

---
+++ js/funciones.js
@@ -0,0 +1,35 @@
+function product(param1, param2) {
+    return param1*param2;
+}
+// nomUser=prompt("Dime tu nombre");
+// resultPlus=product(8,9);
+var numUser1=prompt("Dame un numero");
+var numUser2=prompt("dame otro");
+var resultPlus1=product(numUser1,numUser2);
+// var num2=product(10,5);
+
+alert("el valor de la multiplicacion es "+resultPlus1)
+
+
+// function salute(name) {
+//     alert("good morning" + name);
+// }
+// userName=prompt("Whats your name");
+// salute(userName);
+
+
+// la funcion que te dice cual numero es mas alto
+
+// function numero(cifra1, cifra2) {
+//     if (cifra1>cifra2) {
+//         alert("El numero mas alto es "+ cifra1);
+//     }
+//     else {
+//         alert("El numero mas alto es " + cifra2);
+//     }
+// }
+
+// num1=prompt("Dime un numero");
+// num2=prompt("Dime otro");
+
+// numero(num1, num2);
